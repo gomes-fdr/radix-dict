2508a5cf41660dde046639090451118075d9993f
Fabiano Gomes <frgomes@digicon.com.br>
2015-11-18 09:01:47 -0200

    Initial commit.

---
+++ radix/radix.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    Insertion adds a new string to the trie while trying to minimize
+    the amount of data stored
+"""
+def insert(s):
+    pass
+
+"""
+     Deletion removes a string from the trie
+"""
+def delete(s)
+    pass
+    
+"""
+    Determines if a string exists in a trie
+"""
+def lookup(s)
+    pass
+    
