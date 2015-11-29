eee03c05867b80b7eaec340b292d9f24603f473d
Fabiano da Rosa Gomes <gomes.fdr@gmail.com>
2015-11-29 20:26:50 -0200

    - Refatorado nome de pacote - Modelado classe Radix

---
+++ algoritmos/radix.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class Radix(object):
+    def __init__(self):
+        self._data = {}
+
+    def addWord(self, word):
+        data = self._data
+        i = 0
+        while 1:
+            try:
+                node = data[word[i:i+1]]
+            except KeyError:
+                if data:
+                    data[word[i:i+1]] = [word[i+1:],{}]
+                else:
+                    if word[i:i+1] == '':
+                        return
+                    else:
+                        if i != 0:
+                            data[''] = ['',{}]
+                        data[word[i:i+1]] = [word[i+1:],{}]
+                return
+
+            i += 1
+            if word.startswith(node[0],i):
+                if len(word[i:]) == len(node[0]):
+                    if node[1]:
+                        try:
+                            node[1]['']
+                        except KeyError:
+                            data = node[1]
+                            data[''] = ['',{}]
+                    return
+                else:
+                    i += len(node[0])
+                    data = node[1]
+            else:
+                ii = i
+                j = 0
+                while ii != len(word) and j != len(node[0]) and \
+                      word[ii:ii+1] == node[0][j:j+1]:
+                    ii += 1
+                    j += 1
+                tmpdata = {}
+                tmpdata[node[0][j:j+1]] = [node[0][j+1:],node[1]]
+                tmpdata[word[ii:ii+1]] = [word[ii+1:],{}]
+                data[word[i-1:i]] = [node[0][:j],tmpdata]
+                return
+
+    def isWord(self,word):
+        data = self._data
+        i = 0
+        while 1:
+            try:
+                node = data[word[i:i+1]]
+            except KeyError:
+                return False
+            i += 1
+            if word.startswith(node[0],i):
+                if len(word[i:]) == len(node[0]):
+                    if node[1]:
+                        try:
+                            node[1]['']
+                        except KeyError:
+                            return False
+                    return True
+                else:
+                    i += len(node[0])
+                    data = node[1]
+            else:
+                return False
+
+    def isPrefix(self,word):
+        data = self._data
+        i = 0
+        wordlen = len(word)
+        while 1:
+            try:
+                node = data[word[i:i+1]]
+            except KeyError:
+                return False
+            i += 1
+            if word.startswith(node[0][:wordlen-i],i):
+                if wordlen - i > len(node[0]):
+                    i += len(node[0])
+                    data = node[1]
+                else:
+                    return True
+            else:
+                return False
+
+    def removeWord(self,word):
+        data = self._data
+        i = 0
+        while 1:
+            try:
+                node = data[word[i:i+1]]
+            except KeyError:
+                print "Word is not in trie."
+                return
+            i += 1
+            if word.startswith(node[0],i):
+                if len(word[i:]) == len(node[0]):
+                    if node[1]:
+                        try:
+                            node[1]['']
+                            node[1].pop('')
+                        except KeyError:
+                            print "Word is not in trie."
+                        return
+                    data.pop(word[i-1:i])
+                    return
+                else:
+                    i += len(node[0])
+                    data = node[1]
+            else:
+                print "Word is not in trie."
+                return
+
+
+    __getitem__ = isWord
--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python
+#!/usr/bin/env python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+from algoritmos import Radix
 
 
 def main():
+    
+    x = Radix()
+    
+    x.addWord("abcdef")
+    x.addWord("assunto")
+    x.addWord("atrevido")
+    
+    print x._data
 
     return 0
 
