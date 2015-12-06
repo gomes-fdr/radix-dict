b8792c9a8b707be0a6ccc55ad547a12e8f167e7f
Fabiano da Rosa Gomes <gomes.fdr@gmail.com>
2015-12-06 15:51:03 -0200

    - Código refatorado - Adicionado dicionario e arquivo com texto para teste.

--- algoritmos/radix.py
+++ algoritmos/radix.py
@@ -1,123 +1,50 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from collections import defaultdict
+
 class Radix(object):
+    """
+    Implement a trie with insert, search, and startsWith methods.
+    """
     def __init__(self):
-        self._data = {}
+        self.root = defaultdict()
 
-    def addWord(self, word):
-        data = self._data
-        i = 0
-        while 1:
-            try:
-                node = data[word[i:i+1]]
-            except KeyError:
-                if data:
-                    data[word[i:i+1]] = [word[i+1:],{}]
-                else:
-                    if word[i:i+1] == '':
-                        return
-                    else:
-                        if i != 0:
-                            data[''] = ['',{}]
-                        data[word[i:i+1]] = [word[i+1:],{}]
-                return
+    def insert(self, word):
+        """Inserts a word into the trie."""
+        current = self.root
+        for letter in word.lower():
+            current = current.setdefault(letter, {})
+        current.setdefault("_end")
 
-            i += 1
-            if word.startswith(node[0],i):
-                if len(word[i:]) == len(node[0]):
-                    if node[1]:
-                        try:
-                            node[1]['']
-                        except KeyError:
-                            data = node[1]
-                            data[''] = ['',{}]
-                    return
-                else:
-                    i += len(node[0])
-                    data = node[1]
-            else:
-                ii = i
-                j = 0
-                while ii != len(word) and j != len(node[0]) and \
-                      word[ii:ii+1] == node[0][j:j+1]:
-                    ii += 1
-                    j += 1
-                tmpdata = {}
-                tmpdata[node[0][j:j+1]] = [node[0][j+1:],node[1]]
-                tmpdata[word[ii:ii+1]] = [word[ii+1:],{}]
-                data[word[i-1:i]] = [node[0][:j],tmpdata]
-                return
-
-    def isWord(self,word):
-        data = self._data
-        i = 0
-        while 1:
-            try:
-                node = data[word[i:i+1]]
-            except KeyError:
+    def search(self, word):
+        """Returns True if the word is in the trie."""
+        current = self.root
+        for letter in word.lower():
+            if letter not in current:
                 return False
-            i += 1
-            if word.startswith(node[0],i):
-                if len(word[i:]) == len(node[0]):
-                    if node[1]:
-                        try:
-                            node[1]['']
-                        except KeyError:
-                            return False
-                    return True
-                else:
-                    i += len(node[0])
-                    data = node[1]
+            current = current[letter]
+        if "_end" in current:
+            return True
+        return False
+
+    def startsWith(self, prefix):
+        """
+        Returns True if there is any word in the trie
+        that starts with the given prefix.
+        Returns that find prefix
+        """
+        current = self.root
+        index = 0
+        for letter in prefix.lower():
+            if letter not in current:
+                return (False, None)
             else:
-                return False
-
-    def isPrefix(self,word):
-        data = self._data
-        i = 0
-        wordlen = len(word)
-        while 1:
-            try:
-                node = data[word[i:i+1]]
-            except KeyError:
-                return False
-            i += 1
-            if word.startswith(node[0][:wordlen-i],i):
-                if wordlen - i > len(node[0]):
-                    i += len(node[0])
-                    data = node[1]
-                else:
-                    return True
-            else:
-                return False
-
-    def removeWord(self,word):
-        data = self._data
-        i = 0
-        while 1:
-            try:
-                node = data[word[i:i+1]]
-            except KeyError:
-                print "Word is not in trie."
-                return
-            i += 1
-            if word.startswith(node[0],i):
-                if len(word[i:]) == len(node[0]):
-                    if node[1]:
-                        try:
-                            node[1]['']
-                            node[1].pop('')
-                        except KeyError:
-                            print "Word is not in trie."
-                        return
-                    data.pop(word[i-1:i])
-                    return
-                else:
-                    i += len(node[0])
-                    data = node[1]
-            else:
-                print "Word is not in trie."
-                return
+                current = current[letter]
+                index = index+1
+            
+        #~ print index
+        return (True, prefix[:index+1])
+        
 
 
-    __getitem__ = isWord
--- app.py
+++ app.py
@@ -3,24 +3,49 @@
 
 from algoritmos import Radix
 
+# NOTAS SOBRE ESSE PROGRAMA.
+# Fiquei com um bug no startWith - gostaria de devolver a palavra
+# semelhante encontrada no dicionario, mas ainda não consegui fazer isso
+# pensei em apenas identificar o indice de até onde são iguais e fazer
+# um slice na string recebida, mas não ficou bom...
 
 def main():
     
-    x = Radix()
+    # Loading files content.
+    f_dictionary = open("data/dicionario.txt")
+    f_text = open("data/texto.txt")
     
-    x.addWord("abcdef")
-    x.addWord("assunto")
-    x.addWord("atrevido")
-
-    x.addWord("boa")
-    x.addWord("bolacha")
+    # Create objects to handle with text
+    dictionary = Radix()
     
-    x.addWord("calota")
-    x.addWord("colecao")
+    # Put words from dict file in dict object
+    for word in f_dictionary.read().split():
+        dictionary.insert(word)
     
+    print "Carregando dicionario..."
+    print "...pronto!"
     
-    print x._data
-
+    print ""
+    
+    print "Iniciando correcao do texto..."
+    
+    print ""
+    print ""
+    
+    find, word = dictionary.startsWith("pea")
+    print find, word
+    
+    # Read words from file
+    for word in f_text.read().split():
+        #~ print word
+        if not (dictionary.search(word)):
+            print ("palavra: \"" + word + "\" nao estah no dicionario")
+            continue
+        
+        find, t_word = dictionary.startsWith(word)
+        if(find):
+            print ("Achei algo parecido com: \"" + t_word.lower() + "\" no arquivo com texto")
+        
     return 0
 
 if __name__ == '__main__':
